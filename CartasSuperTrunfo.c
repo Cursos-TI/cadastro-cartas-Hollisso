510c7511361f10b5726b3b2c763d15901ce11cdb
Hollisso <202509077942@alunos.estacio.br>
2025-09-18 17:04:28 +0000

    Versão Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,7 @@ int main() {
 // ---- CARTA 1 ----
 // VARIAVEIS
   char codigo1[10];
+  char cidade1[20];
   float populacao1;
   float area1;
   float PIB1;
@@ -13,12 +14,15 @@ int main() {
   printf("--- Cadastro da carta 01 --- \n\n"); 
   printf("- Código da sua carta: ");
   scanf("%s", codigo1);
+
+  printf("- Qual a sua cidade: ");
+  scanf("%s", cidade1);
  
   printf("- Quantidade da população da sua carta: ");
   scanf("%f", &populacao1);
  
   printf("- Tamanho da área da sua carta: ");
-  scanf("%d", &area1);
+  scanf("%f", &area1);
   
   printf("- Valor do PIB da sua carta: ");
   scanf("%f", &PIB1);
@@ -32,7 +36,8 @@ int main() {
 //---- CARTA 02 ----
 // VARIAVEIS
   char codigo2[10];
-  int populacao2;
+  char cidade2[20];
+  float populacao2;
   float area2;
   float PIB2;
   int pontos_turisticos2;
@@ -41,12 +46,15 @@ int main() {
   printf("--- Cadastro da carta 02 --- \n\n"); 
   printf("- Código da sua carta: ");
   scanf("%s", codigo2);
+
+  printf("- Qual a sua cidade: ");
+  scanf("%s", cidade2);
  
   printf("- Quantidade da população da sua carta: ");
   scanf("%f", &populacao2);
  
   printf("- Tamanho da área da sua carta: ");
-  scanf("%d", &area2);
+  scanf("%f", &area2);
   
   printf("- Valor do PIB da sua carta: ");
   scanf("%f", &PIB2);
@@ -62,20 +70,22 @@ int main() {
 //DADOS CARTA 1
   printf("==== Carta 01 ====\n");
   printf("Código: %s\n", codigo1);
-  printf("População: %d\n", pontos_turisticos1);
-  printf("Área: %d\n", area1);
-  printf("PIB: %d\n", PIB1);
+  printf("Cidade: %s\n", cidade1);
+  printf("População: %f\n", pontos_turisticos1);
+  printf("Área: %f\n", area1);
+  printf("PIB: %f\n", PIB1);
   printf("Pontos Turisticos: %d\n\n", pontos_turisticos1);
 
 //DADOS CARTA 2
   printf("==== Carta 02 ====\n");
   printf("Código: %s\n", codigo2);
-  printf("População: %d\n", populacao2);
-  printf("Área: %d\n", area2);
-  printf("PIB: %d\n", PIB2);
+  printf("Cidade: %s\n", cidade2);
+  printf("População: %f\n", populacao2);
+  printf("Área: %f\n", area2);
+  printf("PIB: %f\n", PIB2);
   printf("Pontos Turisticos: %d\n\n", pontos_turisticos2);
 
-  printf("||  Parabéns, cartas cadastradas com sucesso!  ||");
+  printf("||  Parabéns, cartas cadastradas com sucesso!  ||\n\n");
 
 return 0;
 } 
